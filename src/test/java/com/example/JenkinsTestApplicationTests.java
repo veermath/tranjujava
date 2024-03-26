e89623737bd92b2deb43e7d526bef519432da84a
tranju <tranju664@gmail.com>
2024-03-26 07:16:21 +0530

    adding the files

---
+++ src/test/java/com/example/JenkinsTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 1);
+	}
+
+}
